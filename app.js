54a7cfe74de47c9289846a1d8a03c9000d5538cc
Ovidio R. Calvet <calveto@gmail.com>
2016-01-17 19:05:13 -0500

    Added the click event listener to the click of the stopwatch

--- app.js
+++ app.js
@@ -3,9 +3,19 @@
   // Initialize counter
   var counter = 0;
   
-  // Display counter value inside h1
+  // Get h1 object representation
   var counterDisplay = document.getElementById('counter');
   
+  // Display the initialized value of counter
   counterDisplay.innerText = "Count = " + counter;
+  
+  // Add 'click' event listener
+  var image = document.getElementById('stopwatch');
+  image.addEventListener('click', function() {
+    // increment the counter 
+    counter = counter + 1;
+    // display the new count
+    counterDisplay.innerText = "Count = " + counter;    
+  });
   
 })(document);
