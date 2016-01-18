0998673d9b922c6d521220ec54db255cf81605a1
Ovidio R. Calvet <calveto@gmail.com>
2016-01-17 19:01:27 -0500

    initialized the counter and got a reference to the h1 dom element

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+(function(document) {
+  
+  // Initialize counter
+  var counter = 0;
+  
+  // Display counter value inside h1
+  var counterDisplay = document.getElementById('counter');
+  
+  counterDisplay.innerText = "Count = " + counter;
+  
+})(document);
